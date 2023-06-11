07a25feb15f6c48ab5e24a6b7e68e9a025a902f6
Christiaan de Kock <christiaandk96@gmail.com>
2023-06-11 15:45:12 +0200

    property binding and interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector: '.app-server',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html', 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
 }
